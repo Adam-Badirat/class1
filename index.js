817eb6688763ff4cffbb5f0936227b4403264579
Adam Badirat <badiratadam249@gmail.com>
2026-09-24 09:24:29 +0100

    fix render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const mongoose = require("mongoose")
 require("dotenv").config();
-const port = process.env.port;
+const port = process.env.PORT;
 
 
 const student = [
